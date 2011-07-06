8fe5c4ac4d1b8a7623ba3b890a0e5a6129ff9a2d
Rupert Voelcker <rupert.voelcker@bt.com>
2011-07-06 11:13:16 +0100

    Ensure queries returning an array can be registered

--- spec/register_query_spec.rb
+++ spec/register_query_spec.rb
@@ -6,4 +6,9 @@ describe "registering queries" do
     FakeNetLdap.register_query('with_hash_response', {"name" => "Fred Blogs"})
     FakeNetLdap.query_registered?('with_hash_response').should be_true
   end
+
+  it "should allow queries to be registered to respond with an array (for multiple responses)" do
+    FakeNetLdap.register_query('with_array_response', [{"name" => "Fred Blogs"}, {"name" => "John Smith"}])
+    FakeNetLdap.query_registered?('with_array_response').should be_true
+  end
 end
